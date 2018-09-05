bc6f96875bd85f51b5f37c68c3258ff916ea47ac
cjh0605 <junghwn0605@naver.com>
2018-09-05 17:54:38 +0900

    차 : 글 작성

--- aprilOne/src/main/java/kr/or/nextit/notice/service/NoticeService.java
+++ aprilOne/src/main/java/kr/or/nextit/notice/service/NoticeService.java
@@ -1,12 +1,18 @@
 package kr.or.nextit.notice.service;
 
 import java.util.List;
+import java.util.Map;
 
 import kr.or.nextit.comm.model.NoticeVo;
 
 public interface NoticeService {
 	
+	// 리스트
 	public List<NoticeVo> selectNoticeList() throws Exception;
+	
+	// 글작성
+	
+	public void insertNitice(Map<String, Object> hmap) throws Exception;
 	
 	
 
--- aprilOne/src/main/java/kr/or/nextit/notice/service/impl/NoticeMapper.java
+++ aprilOne/src/main/java/kr/or/nextit/notice/service/impl/NoticeMapper.java
@@ -1,6 +1,7 @@
 package kr.or.nextit.notice.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import kr.or.nextit.comm.model.NoticeVo;
 
@@ -8,5 +9,8 @@ public interface NoticeMapper {
 
 	// 매퍼- 게시판 목록을 불러온다
 	public List<NoticeVo> selectNoticeList() throws Exception;
+	
+	// 글작성
+	public void insertNotice(Map<String, Object> hmap) throws Exception;
 
 }

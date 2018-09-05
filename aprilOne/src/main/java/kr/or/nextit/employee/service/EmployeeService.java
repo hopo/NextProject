f0ce471e9d98895bca1d9f8a6ce4ba67c6b4a38b
eunbyeolkim <keb636@naver.com>
2018-09-05 14:03:43 +0900

    0905 kim

---
+++ aprilOne/src/main/java/kr/or/nextit/employee/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package kr.or.nextit.employee.service;
+
+import kr.or.nextit.comm.model.EmployeeVo;
+
+public interface EmployeeService {
+	
+	// 직원등록 	
+	public void employeeInsert(EmployeeVo employeeVo) throws Exception;
+	
+	
+
+}
---
+++ aprilOne/src/main/java/kr/or/nextit/employee/service/impl/EmployeeMapper.java
@@ -0,0 +1,12 @@
+package kr.or.nextit.employee.service.impl;
+
+import kr.or.nextit.comm.model.EmployeeVo;
+
+public interface EmployeeMapper {
+	
+	// 직원등록 	
+		public void employeeInsert(EmployeeVo employeevo) throws Exception;
+		
+	
+
+}
